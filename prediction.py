51dac11f6360662ed5b155d6607ece9ee0ab2813
Tast20 <51784499+Tast20@users.noreply.github.com>
2019-07-30 17:31:09 +0300

    Add files via upload

---
+++ prediction.py
@@ -0,0 +1,16 @@
+import pickle
+def makeTokens(f):
+    tokens = []
+    for i in f:
+        tokens.append(i)
+    return tokens
+def get_security(password):
+    model = pickle.load(open('lstm_model.sav', 'rb'))
+    vectorizer = pickle.load(open('vectorizer.sav', 'rb'))
+    X_predict = []
+    X_predict.append(password)
+    X_predict = vectorizer .transform(X_predict)
+    y_Predict = model.predict_classes(X_predict)
+    return int(y_Predict)
+
+
